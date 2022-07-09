c9b0b22ce5c38c914935b1f4c00b597f09311ed2
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-07-09 18:49:33 -0300

    Add modules

---
+++ modules/Books.js
@@ -0,0 +1,11 @@
+// Books array for the book objects with two methods, add book and remove book
+
+export default class Books {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook = (book) => this.books.push(book);
+
+  removeBook = (bookTitle) => this.books = this.books.filter((book) => book.title !== bookTitle);
+}
